f848aed21d9261d3f1c83a036bb414e2d531ab53
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-28 17:21:28 +0330

    feat(page.tsx): enhance Home component with new button designs and loading states for Persian language support

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <div>
-      <main>
-        <h2 className="text-red-500">new tailwind version added</h2>
+      <main className="flex flex-col gap-4 p-4">
+        <h2 className="text-gray-500">طراحی دکمه :</h2>
+        <div className="max-w-[300px]">
+          <Button>بریم تمومش کنیم</Button>
+        </div>
+        <h2 className="text-gray-500">وقتی لودینگه :</h2>
+        <div className="max-w-[300px]">
+          <Button disabled>بریم تمومش کنیم</Button>
+        </div>
+        <hr />
+        <h2 className="text-gray-500">تمام عرض :</h2>
+        <div className="max-w-[300px]">
+          <Button fullWidth>بریم تمومش کنیم</Button>
+        </div>
+        <h2 className="text-gray-500">وقتی تمام عرض و لودینگه :</h2>
+        <div className="max-w-[300px]">
+          <Button disabled fullWidth>
+            بریم تمومش کنیم
+          </Button>
+        </div>
       </main>
     </div>
   );
